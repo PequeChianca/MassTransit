295988c4d3438f8e84438de7f971b03c3ab89f2a
Pedro Henrique <phenrique@telemedicineclinic.onmicrosoft.com>
2023-05-08 11:14:44 +0100

    initial commit

---
+++ GettingStarted/Worker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+
+namespace GettingStarted
+{
+    public class Worker : BackgroundService
+    {
+        readonly IBus _bus;
+
+        protected override Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+
+        }
+
+    }
+}
